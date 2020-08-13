ea9230b5c4c582efaf119ad0a36dc9a1fb1cfebd
ANeshkova <anitowurth@abv.bg>
2020-08-13 22:07:01 +0100

    Added solution

---
+++ 03.1 Conditional Statements - Lab/02. GreaterNumber.js
@@ -0,0 +1,11 @@
+function greaterNumber(first, second) {
+
+    if (Number(first) >= Number(second)) {
+        console.log(first);
+
+    } else {
+        console.log(second);
+    }
+}
+
+greaterNumber("3", "5");
