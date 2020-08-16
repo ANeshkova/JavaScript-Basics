237c1acc7da3e229d9f8c16209c16ff019deacd5
ANeshkova <anitowurth@abv.bg>
2020-08-16 23:56:30 +0300

    Added solution

---
+++ 04.1 Nested Conditional Statements - Lab/03. PersonalTitles.js
@@ -0,0 +1,20 @@
+function personalTitles(age, gender) {
+
+    age = Number(age);
+
+    if (gender == 'm') {
+        if (age < 16) {
+            console.log("Master");
+        } else {
+            console.log("Mr.");
+        }
+    } else {
+        if (age < 16) {
+            console.log("Miss");
+        } else {
+            console.log("Ms.");
+        }
+    }
+}
+
+personalTitles(12, 'f');
