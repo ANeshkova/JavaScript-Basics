2b502ce4669acda55a85f2ff3f1108e1130e19ed
ANeshkova <anitowurth@abv.bg>
2020-08-18 22:56:14 +0100

    Added solution

---
+++ 04.2 Nested Conditional Statements - Exercise/03. SummerOutfit.js
@@ -0,0 +1,52 @@
+function summerOutfit(degrees, partOfTheDay) {
+
+    degrees = Number(degrees);
+
+    let outfit = "";
+    let shoes = "";
+
+    if (degrees >= 10 && degrees <= 18) {
+        if (partOfTheDay == "Morning") {
+            outfit = "Sweatshirt";
+            shoes = "Sneakers";
+
+        } else if (partOfTheDay == "Afternoon") {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+
+        } else {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        }
+    } else if (degrees > 18 && degrees <= 24) {
+        if (partOfTheDay == "Morning") {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+
+        } else if (partOfTheDay == "Afternoon") {
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+
+        } else {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        }
+    } else {
+        if (partOfTheDay == "Morning") {
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+
+        } else if (partOfTheDay == "Afternoon") {
+            outfit = "Swim Suit";
+            shoes = "Barefoot";
+
+        } else {
+            outfit = "Shirt";
+            shoes = "Moccasins";
+        }
+    }
+
+    console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+}
+
+summerOutfit("16", "Morning");
