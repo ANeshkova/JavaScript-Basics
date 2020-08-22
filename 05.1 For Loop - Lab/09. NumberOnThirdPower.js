76aa22281f966736a4f2be4ad558a03813a16c0a
ANeshkova <anitowurth@abv.bg>
2020-08-22 14:30:07 +0100

    Added solution

---
+++ 05.1 For Loop - Lab/09. NumberOnThirdPower.js
@@ -0,0 +1,18 @@
+function numberOnThirdPower(n) {
+
+    n = Number(n);
+
+    let start = 0;
+    
+    if (n % 2 == 0) {
+        start = 2;
+    } else {
+        start = 1;
+    }
+
+    for (let i = start; i <= n; i += 2) {
+        console.log(`Current number: ${i}. Cube: ${i * i * i}`);
+    }
+}
+
+numberOnThirdPower2("5");
